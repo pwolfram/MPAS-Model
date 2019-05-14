4331ad028ae4b0f22a37cb94230868adde2e38a1
Phillip J. Wolfram <phillipwolfram@gmail.com>
2019-05-14 09:38:22 -0600

    More robust particle testing approach

--- testing_and_setup/compass/ocean/soma/analysis/remove_proc_particles.py
+++ testing_and_setup/compass/ocean/soma/analysis/remove_proc_particles.py
@@ -2,12 +2,13 @@
 
 import xarray as xr
 import numpy as np
+import sys
 
-ds = xr.open_dataset('particles_old.nc')
+ds = xr.open_dataset(sys.argv[1])
 
 # remove particles on a processor and output new file
 dsnew = ds.where(ds.currentBlock != 0, drop=True)
-dsnew.to_netcdf('particles.nc')
+dsnew.to_netcdf(sys.argv[2], format='NETCDF3_CLASSIC')
 
 assert (0 not in np.unique(dsnew.currentBlock)), \
         'Error, still have particles on processor 0!'
